535912bc7c34d0ba06b0fae92d0b4261a8c10070
Tishran <rushan.hamatyrov@mail.ru>
2024-03-27 19:35:02 +0300

    refactored hw hooch array

--- memory/hugearray/handler.c
+++ memory/hugearray/handler.c
@@ -6,38 +6,28 @@
 
 extern size_t PAGE_SIZE;
 extern double *SQRTS;
-extern int MAX_SQRTS;
 
 void CalculateSqrts(double *sqrt_pos, int start, int n);
 
-//debug variable
-int idx = 0;
-
 void HandleSigsegv(int sig, siginfo_t *siginfo, void *ctx) {
-//    fprintf(stderr, "%s: %zu\n", "handle sigsev func", idx * PAGE_SIZE);
-    ++idx;
     void *fault_address = siginfo->si_addr;
     size_t offset = (size_t) (fault_address - (void *) SQRTS);
 
-    size_t page_number = offset / PAGE_SIZE;
-    void *page_start = mmap((void *) SQRTS + PAGE_SIZE * page_number, PAGE_SIZE,
+    int page_number = offset / PAGE_SIZE;
+    void* addr = (void*) SQRTS + page_number * PAGE_SIZE;
+    void *page_start = mmap(addr, PAGE_SIZE,
                             PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
 
     if (page_start == MAP_FAILED) {
         if (errno == ENOMEM) {
-//            fprintf(stderr, "%s\n", "NO MEM HAPPENED!!!!!!!!!!!!!!!!!!!!!!!!!!");
-            munmap((void*) SQRTS, PAGE_SIZE * page_number);
-//            SQRTS = (double *) ((void *) SQRTS + page_number * PAGE_SIZE);
-            page_start = mmap((void *) SQRTS + PAGE_SIZE * page_number, PAGE_SIZE,
+            munmap((void*) SQRTS, page_number * PAGE_SIZE);
+            page_start = mmap(addr, PAGE_SIZE,
                                     PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
-//            fprintf(stderr, "%d %d\n", page_start == MAP_FAILED, errno == ENOMEM);
         } else {
             perror("mmap error");
             exit(EXIT_FAILURE);
         }
     }
 
-    int start_value = page_number * PAGE_SIZE;
-//    fprintf(stderr, "%d\n", 40);
-    CalculateSqrts(page_start, start_value / sizeof(double), PAGE_SIZE / sizeof(double));
+    CalculateSqrts(page_start, page_number * PAGE_SIZE / sizeof(double), PAGE_SIZE / sizeof(double));
 }
