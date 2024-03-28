7af501cbe1d722c5c13df33584536bffc5883e2e
Tishran <rushan.hamatyrov@mail.ru>
2024-03-28 19:50:05 +0300

    chech exit bug

--- memory/hugearray/handler.c
+++ memory/hugearray/handler.c
@@ -10,24 +10,25 @@ extern double *SQRTS;
 void CalculateSqrts(double *sqrt_pos, int start, int n);
 
 void HandleSigsegv(int sig, siginfo_t *siginfo, void *ctx) {
-    void *fault_address = siginfo->si_addr;
-    size_t offset = (size_t) (fault_address - (void *) SQRTS);
-
-    int page_number = offset / PAGE_SIZE;
-    void* addr = (void*) SQRTS + page_number * PAGE_SIZE;
-    void *page_start = mmap(addr, PAGE_SIZE,
-                            PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
-
-    if (page_start == MAP_FAILED) {
-        if (errno == ENOMEM) {
-            munmap((void*) SQRTS, page_number * PAGE_SIZE);
-            page_start = mmap(addr, PAGE_SIZE,
-                                    PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
-        } else {
-            perror("mmap error");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    CalculateSqrts(page_start, page_number * PAGE_SIZE / sizeof(double), PAGE_SIZE / sizeof(double));
+    exit(0);
+//    void *fault_address = siginfo->si_addr;
+//    size_t offset = (size_t) (fault_address - (void *) SQRTS);
+//
+//    int page_number = offset / PAGE_SIZE;
+//    void* addr = (void*) SQRTS + page_number * PAGE_SIZE;
+//    void *page_start = mmap(addr, PAGE_SIZE,
+//                            PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+//
+//    if (page_start == MAP_FAILED) {
+//        if (errno == ENOMEM) {
+//            munmap((void*) SQRTS, page_number * PAGE_SIZE);
+//            page_start = mmap(addr, PAGE_SIZE,
+//                                    PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+//        } else {
+//            perror("mmap error");
+//            exit(EXIT_FAILURE);
+//        }
+//    }
+//
+//    CalculateSqrts(page_start, page_number * PAGE_SIZE / sizeof(double), PAGE_SIZE / sizeof(double));
 }
