8c27abd686d028616c9dcf514d8239471aa60dc1
Tishran <rushan.hamatyrov@mail.ru>
2024-04-02 21:33:19 +0300

    spinlock hw, but why works tho

--- atomic/spinlock/spinlock.h
+++ atomic/spinlock/spinlock.h
@@ -1,13 +1,34 @@
 #pragma once
 
 struct SpinLock {
+    int curr_state;
 };
 
-inline void SpinLock_Init(struct SpinLock* lock) {
+inline void SpinLock_Init(struct SpinLock *lock) {
+    lock->curr_state = 0;
 }
 
-inline void SpinLock_Lock(struct SpinLock* lock) {
+inline void SpinLock_Lock(struct SpinLock *lock) {
+    int unlocked = 0;
+    int locked = 1;
+
+    asm volatile("while_lock:\n\t"
+                 "movl $0, %%eax\n\t"
+                 "lock\n\t"
+                 "cmpxchg %2, %0\n\t"
+                 "jne while_lock"
+            : "+m" (lock->curr_state), "+a" (unlocked)
+            : "r" (locked)
+            : "memory");
+
+    __sync_synchronize();
 }
 
-inline void SpinLock_Unlock(struct SpinLock* lock) {
+inline void SpinLock_Unlock(struct SpinLock *lock) {
+    __sync_synchronize();
+    int unlocked = 0;
+    asm volatile("xchg %0, %1"
+            : "+r" (lock->curr_state), "+r" (unlocked)
+            :
+            : "memory");
 }
