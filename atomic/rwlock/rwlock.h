aff105a5a22f69f769b9a27abb8dc2eb96e3a58c
Tishran <rushan.hamatyrov@mail.ru>
2024-04-03 22:19:29 +0300

    wtf2

--- atomic/rwlock/rwlock.h
+++ atomic/rwlock/rwlock.h
@@ -24,7 +24,7 @@ inline void RwLock_Init(struct RwLock *lock) {
 }
 
 inline void RwLock_ReadLock(struct RwLock *lock) {
-    usleep(500000);
+    usleep(500);
     SpinLock_Lock(&lock->spinlock);
     AtomicXchg(&lock->readers, lock->readers);
 
