001e2b1673aad0ea774a8759e6a8ea97cb75f3eb
Tishran <rushan.hamatyrov@mail.ru>
2024-04-04 00:04:51 +0300

    solution with atomics

--- atomic/rwlock/rwlock.h
+++ atomic/rwlock/rwlock.h
@@ -1,52 +1,44 @@
 #pragma once
 
-#include <stdbool.h>
-#include <unistd.h>
-#include <asm/unistd_64.h>
-
 #include "spinlock.h"
 #include "atomic.h"
 
-//enum rwlockstate {
-//    RLOCK, WLOCK, UNLOCK
-//};
-//int64_t UNLOCK = 0;
-//int64_t RLOCK = 1;
-//int64_t WLOCK = -1;
+const int64_t UNLOCK = 0;
+const int64_t RLOCK = 1;
+const int64_t WLOCK = -1;
 
 struct RwLock {
-    int64_t writing;
-    int64_t readers;
+    int64_t state;
 };
 
 inline void RwLock_Init(struct RwLock *lock) {
-    lock->writing = 0; // -1 - writing is permitted
-    lock->readers = 0;
+    lock->state = UNLOCK;
 }
 
 inline void RwLock_ReadLock(struct RwLock *lock) {
-    int64_t expected_writing = 0;
-    while (AtomicCas(&lock->writing, &expected_writing, -1) != 1) {
+    int64_t expected_unlock = UNLOCK;
+    int64_t expected_rlock = RLOCK;
+    while (AtomicCas(&lock->state, &expected_unlock, RLOCK) != 1 ||
+           AtomicCas(&lock->state, &expected_rlock, RLOCK) != 1) {
         asm volatile("pause");
     }
 
-    AtomicAdd(&lock->readers, 1);
+    AtomicAdd(&lock->state, 1);
 }
 
 inline void RwLock_ReadUnlock(struct RwLock *lock) {
-    AtomicAdd(&lock->writing, 1);
-    AtomicSub(&lock->readers, 1);
+    int64_t expected_readers_cnt = 2;
+    AtomicCas(&lock->state, &expected_readers_cnt, RLOCK);
+    AtomicSub(&lock->state, 1);
 }
 
 inline void RwLock_WriteLock(struct RwLock *lock) {
-    int64_t expected_reading = 0;
-    int64_t expected_writing = 0;
-    while (AtomicCas(&lock->readers, &expected_reading, 0) != 1 &&
-           AtomicCas(&lock->writing, &expected_writing, 1) != 1) {
+    int64_t expected_unlock = UNLOCK;
+    while (AtomicCas(&lock->state, &expected_unlock, WLOCK) != 1) {
         asm volatile("pause");
     }
 }
 
 inline void RwLock_WriteUnlock(struct RwLock *lock) {
-    AtomicSub(&lock->writing, 1);
+    AtomicXchg(&lock->state, UNLOCK);
 }
