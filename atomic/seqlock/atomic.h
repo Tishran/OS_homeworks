0e75ebe34f1a8d37798824bf4a32511e9135ed6f
Tishran <rushan.hamatyrov@mail.ru>
2024-04-06 03:18:37 +0300

    final lock free

--- atomic/seqlock/atomic.h
+++ atomic/seqlock/atomic.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-inline void AtomicAdd(int64_t *atomic, int64_t value) {
+inline void AtomicAdd(volatile int64_t *atomic, int64_t value) {
     asm volatile("lock\n\t"
                  "add %1, %0\n\t"
             : "+m" (*atomic)
@@ -12,7 +12,7 @@ inline void AtomicAdd(int64_t *atomic, int64_t value) {
             : "memory");
 }
 
-inline void AtomicSub(int64_t *atomic, int64_t value) {
+inline void AtomicSub(volatile int64_t *atomic, int64_t value) {
     asm volatile("lock\n\t"
                  "sub %1, %0\n\t"
             : "+m" (*atomic)
@@ -20,7 +20,7 @@ inline void AtomicSub(int64_t *atomic, int64_t value) {
             : "memory");
 }
 
-inline int64_t AtomicXchg(int64_t *atomic, int64_t value) {
+inline int64_t AtomicXchg(volatile int64_t *atomic, int64_t value) {
     asm volatile("xchg %1, %0\n\t"
             : "+m" (*atomic), "+r" (value)
             :
@@ -29,7 +29,7 @@ inline int64_t AtomicXchg(int64_t *atomic, int64_t value) {
     return value;
 }
 
-inline int64_t AtomicCas(int64_t *atomic, int64_t *expected, int64_t value) {
+inline int64_t AtomicCas(volatile int64_t *atomic, int64_t *expected, int64_t value) {
     bool result = 0;
     asm volatile("lock\n\t"
                  "cmpxchgq %3, %1\n\t"
--- atomic/seqlock/seqlock.h
+++ atomic/seqlock/seqlock.h
@@ -8,7 +8,7 @@
 #include <unistd.h>
 
 struct SeqLock {
-    int64_t state;
+    volatile int64_t state;
     struct SpinLock lock;
 };
 
