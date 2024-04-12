4cccb9c5ceb2a9085362d4b73f95df6ffb073cc6
Tishran <rushan.hamatyrov@mail.ru>
2024-04-12 21:22:09 +0300

    dummy try

--- syscall/gate/kern/sysgate.c
+++ syscall/gate/kern/sysgate.c
@@ -3,5 +3,29 @@
 
 // Your code here
 
+inline long read_msr(int msr) {
+    long value;
+    asm volatile ( "rdmsr" : "=A" (value) : "c" (msr) );
+
+    return value;
+}
+
+inline void write_msr(int msr, long value) {
+//    int low = value & 0xFFFFFFFF;
+//    int high = value >> 32;
+//    asm volatile (
+//            "wrmsr"
+//            :
+//            : "c"(msr), "a"(low), "d"(high)
+//            );
+    asm volatile ( "wrmsr" : : "c" (msr), "A" (value) );
+}
+
+void _syscall_enter();
+
 void sysgate() {
+    write_msr(IA32_EFER, read_msr(IA32_EFER) + 1); // это точно выставит нулевой бит? кажется да
+    write_msr(IA32_LSTAR, (long) _syscall_enter);
+
+    asm volatile("sysretq");
 }
