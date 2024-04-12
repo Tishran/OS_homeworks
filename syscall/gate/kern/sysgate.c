26eb1c4a70aac4fb4ccb383e93beca6216c50be0
Tishran <rushan.hamatyrov@mail.ru>
2024-04-13 01:30:41 +0300

    dummy commit

--- syscall/gate/kern/sysgate.c
+++ syscall/gate/kern/sysgate.c
@@ -1,31 +1,34 @@
 #include <kern/sysgate.h>
 #include <kern/syscall.h>
 
-// Your code here
+extern void _start_user();
 
 inline long read_msr(int msr) {
     long value;
-    asm volatile ( "rdmsr" : "=A" (value) : "c" (msr) );
+    asm volatile ( "rdmsr" : "=A" (value) : "c" (msr));
 
     return value;
 }
 
 inline void write_msr(int msr, long value) {
-//    int low = value & 0xFFFFFFFF;
-//    int high = value >> 32;
-//    asm volatile (
-//            "wrmsr"
-//            :
-//            : "c"(msr), "a"(low), "d"(high)
-//            );
-    asm volatile ( "wrmsr" : : "c" (msr), "A" (value) );
+    int low = value & 0xFFFFFFFF;
+    int high = value >> 32;
+    asm volatile (
+            "wrmsr"
+            :
+            : "c"(msr), "a"(low), "d"(high)
+            );
 }
 
 void _syscall_enter();
 
 void sysgate() {
-    write_msr(IA32_EFER, read_msr(IA32_EFER) + 1); // это точно выставит нулевой бит? кажется да
+    write_msr(IA32_EFER, read_msr(IA32_EFER) + 1);
     write_msr(IA32_LSTAR, (long) _syscall_enter);
 
-    asm volatile("sysretq");
+    asm volatile("mov %0, %%rcx\n\t"
+                 "sysretq"
+            :
+            : "r" (&_start_user)
+            : "rcx");
 }
